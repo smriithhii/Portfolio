128cfc9321c561f7a3abdba812ca9e68f5751170
smriithhii <107065605+smriithhii@users.noreply.github.com>
2023-10-23 15:16:26 +0530

    Add files via upload

---
+++ func.js
@@ -0,0 +1,19 @@
+var tablinks = document.getElementsByClassName("tab-links");
+var tabcontents = document.getElementsByClassName("tab-contents");
+
+function opentab(tabname) {
+    for (tablink of tablinks) {
+        tablink.classList.remove("active-link");
+    }
+    for (tabcontent of tabcontents) {
+        tabcontent.classList.remove("active-tab");
+    }
+    event.currentTarget.classList.add("active-link");
+    document.getElementById(tabname).classList.add("active-tab");
+}
+function toggle(){
+    var blur = document.getElementById('blur')
+    blur.classList.toggle('active')
+    var popup = document.getElementById('popup')
+    popup.classList.toggle('active')
+}
